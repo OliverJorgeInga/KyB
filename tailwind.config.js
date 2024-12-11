28326d9ebc9541d0852ac4f14834cb8bbc38138e
Ricardo Jorge Inga <oliver.jorge.inga@gmail.com>
2024-12-11 18:43:03 -0500

    estructura html

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'transparent':'#01010133',
+        'red-navi':'#D00000',
+      },
+    },
+  },
+  plugins: [],
+}
+
